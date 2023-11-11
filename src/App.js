bf997fde622b04d7a35a8b45bf25a96c060bc62a
Ivan Iliev <ivanilievdackel@gmail.com>
2023-11-11 15:19:20 +0200

    Navbar structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
-  return <></>;
+  const path = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <>
+          <Navbar />
+        </>
+      ),
+    },
+  ]);
+  return (
+    <>
+      <div className="App">
+        <RouterProvider router={path} />{' '}
+      </div>
+    </>
+  );
 }
 
 export default App;
